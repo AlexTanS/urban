f27f07b9791c8da31187ff1359ec1befe1b264b2
Alex <60185787+AlexTanS@users.noreply.github.com>
2024-07-24 11:12:11 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if not second:
+        return "Ошбика."
+    return first / second
+
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+
+def divide(first, second):
+    if not second:
+        return inf
+    return first / second
