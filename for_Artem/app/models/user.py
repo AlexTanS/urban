784744c8df3cd983e1e3fbcb4ff0b7ed09905bf7
Alex <60185787+AlexTanS@users.noreply.github.com>
2024-12-10 22:41:23 +0300

    Add files via upload

---
+++ for_Artem/app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy import Integer, String, Column
+from app.backend.db import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)  # целое число, первичный ключ, с индексом
+    username = Column(String)  # строка
+    firstname = Column(String)  # строка
+    lastname = Column(String)  # строка
+    age = Column(Integer)  # целое число
+    slug = Column(String, unique=True, index=True)  # строка, уникальная, с индексом
+
+    tasks = relationship("Task",
+                         back_populates="user")  # объект связи с таблицей с таблицей Task, где back_populates='user'
